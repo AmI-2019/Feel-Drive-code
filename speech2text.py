1d9969fc0a3f873c606046c467a93b4c750346b6
Marco Vinai <45501717+vina97@users.noreply.github.com>
2019-05-20 11:51:51 +0200

    add speech2text

---
+++ speech2text.py
@@ -0,0 +1,48 @@
+import speech_recognition as sr
+#import keyboard
+import pyttsx3
+
+engine = pyttsx3.init()
+r = sr.Recognizer()
+mic = sr.Microphone()
+
+
+def recognize():
+    with mic as source:
+        audio = r.listen(source)
+        # print(r.recognize_houndify(audio, "4ld9WM_kTXLshYS-GOKr7g==", "5b55t3QsJeptvgRe6f2U3mhjx9DGMkliWpaG24QVlbEziEAKDKdBlfPcuz037fk9e2UmaDK3-NaNKm7c3ZA6pw=="))
+        return r.recognize_google(audio)
+
+def stopSong():
+    print("stop")
+
+
+def changeSong():
+    print("change")
+
+
+def askSong():
+    engine.say("Do you like this song?")
+    engine.runAndWait()
+    print("done")
+    response = recognize()
+    print(response)
+
+
+def playSong(string):
+    print(string[5:])
+
+
+if __name__ == '__main__':
+    askSong()
+    command = recognize()
+    if command == "exit":
+        exit()
+    elif command == "stop":
+        stopSong()
+    elif command == "change song":
+        changeSong()
+    elif command.startswith("play"):
+        playSong(command)
+
+
