234c7233c7b9d576d916c4cce31cc067329be34d
seandi <eandi.simo@outlook.it>
2019-06-06 19:02:10 +0200

    bug fix

--- Pi-Code/hue_controller_tester.py
+++ Pi-Code/hue_controller_tester.py
@@ -9,6 +9,6 @@ from config import FER_SERVER, HUE_BASE_URL, HUE_USERNAME
 if __name__ == '__main__':
     predictions = requests.get(FER_SERVER+'/predictions').json()
     hue_controller = HueController()
-    lights = HueLights(BASE_URL, USERNAME)
+    lights = HueLights(HUE_BASE_URL, HUE_USERNAME)
     hue = hue_controller.compute_hue(predictions)
     lights.set(hue, 85)
