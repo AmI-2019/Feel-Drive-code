1b04dff7c59f0d1d09350b5966d44d74d2dc3624
seandi <eandi.simo@outlook.it>
2019-06-06 12:23:12 +0200

    code clean up

--- web_application/db_interaction.py
+++ web_application/db_interaction.py
@@ -1,37 +1,45 @@
 import pymysql
 import os
 
+DB_USER = 'root'
+DB_PASSWORD = ''
+DB_HOST = 'localhost'
+
+
 def check_user_password(u,p):
     sql="""SELECT * FROM users 
     WHERE username=%s AND password=%s
      """
-    conn=pymysql.connect(user = 'root', password='purti97',host='localhost',database='feel&drive')
+    conn = pymysql.connect(user=DB_USER, password=DB_PASSWORD, host=DB_HOST, database='feel&drive')
     cursor=conn.cursor()
     cursor.execute(sql,(u,p))
     result=cursor.fetchone()
     conn.close()
     return result
 
+
 def check_user(u):
     sql="""SELECT username FROM users 
     WHERE username=%s
      """
-    conn=pymysql.connect(user = 'root', password='purti97',host='localhost',database='feel&drive')
+    conn = pymysql.connect(user=DB_USER, password=DB_PASSWORD, host=DB_HOST, database='feel&drive')
     cursor=conn.cursor()
     cursor.execute(sql,(u,))
     result=cursor.fetchone()
     conn.close()
     return result
 
+
 def add_user(u,p):
     sql="""INSERT INTO users(username, password) 
     VALUES (%s,%s)
      """
-    conn=pymysql.connect(user = 'root', password='purti97',host='localhost',database='feel&drive')
+    conn = pymysql.connect(user=DB_USER, password=DB_PASSWORD, host=DB_HOST, database='feel&drive')
     cursor=conn.cursor()
     cursor.execute(sql,(u,p))
     conn.commit()
     conn.close()
+
 
 def add_song(song, feeling):
 
@@ -39,17 +47,18 @@ def add_song(song, feeling):
         sql="""INSERT INTO songs(title, feeling)
         VALUES (%s,%s)
         """
-        conn = pymysql.connect(user='root', password='purti97', host='localhost', database='feel&drive')
+        conn = pymysql.connect(user=DB_USER, password=DB_PASSWORD, host=DB_HOST, database='feel&drive')
         cursor = conn.cursor()
         cursor.execute(sql, (song, feeling))
         conn.commit()
         conn.close()
 
+
 def get_song(song):
     sql="""SELECT * FROM songs 
     WHERE title=%s
     """
-    conn = pymysql.connect(user='root', password='purti97', host='localhost', database='feel&drive')
+    conn = pymysql.connect(user=DB_USER, password=DB_PASSWORD, host=DB_HOST, database='feel&drive')
     cursor = conn.cursor()
     cursor.execute(sql, (song,))
     result = cursor.fetchone()
@@ -61,7 +70,7 @@ def add_relation(username,song, liked, feeling):
     sql="""INSERT INTO relations(username, title, liked, feeling) 
     VALUES (%s,%s,%s,%s)
      """
-    conn=pymysql.connect(user = 'root', password='purti97',host='localhost',database='feel&drive')
+    conn = pymysql.connect(user=DB_USER, password=DB_PASSWORD, host=DB_HOST, database='feel&drive')
     cursor=conn.cursor()
     cursor.execute(sql,(username,song,liked, feeling))
     conn.commit()
@@ -72,18 +81,19 @@ def get_liked_songs_by_feeling(username, feeling):
     sql="""SELECT song_title FROM relations
     WHERE feeling=%s AND username=%s
     """
-    conn = pymysql.connect(user='root', password='purti97', host='localhost', database='feel&drive')
+    conn = pymysql.connect(user=DB_USER, password=DB_PASSWORD, host=DB_HOST, database='feel&drive')
     cursor = conn.cursor()
     cursor.execute(sql, (feeling,username))
     result = cursor.fetchall()
     conn.close()
     return result
 
+
 def get_songs_by_feeling( feeling):
     sql="""SELECT title FROM songs
     WHERE feeling=%s
     """
-    conn = pymysql.connect(user='root', password='purti97', host='localhost', database='feel&drive')
+    conn = pymysql.connect(user=DB_USER, password=DB_PASSWORD, host=DB_HOST, database='feel&drive')
     cursor = conn.cursor()
     cursor.execute(sql, (feeling,))
     result = cursor.fetchall()
@@ -96,9 +106,8 @@ def load_db():
    os.chdir(dir)
    for file in os.listdir(dir):
        if file.endswith('.mp3'):
-            add_song(file,'Sadness')
-
+            add_song(file, 'Sadness')
 
 
 if __name__ == '__main__':
-    load_db()
+    get_songs_by_feeling("Sadness")
---
+++ web_application/feel_drive_relations.sql
@@ -0,0 +1,95 @@
+create table songs
+(
+  title   varchar(100) not null
+    primary key,
+  feeling varchar(50)  not null
+);
+
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Bollicine - Vasco Rossi.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Boombastic - Shaggy.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Buffalo Soldier - Bob Marley and the Wailers.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Check yo Self Remix - Ice Cube.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('City of Angels - 30 Seconds to Mars.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Claudia Lewis - M83.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Completamente - Thegiornalisti.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Could You Be Loved - Bob Marley and the Wailers.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Dancing Queen - Abba.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Deviazioni - Vasco Rossi.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Disperato - Thegiornalisti.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Dont Stop Me Now - Queen.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Everybody hurts - REM.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Exodus - Bob Marley and the Wailers.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Fatto Di Te - Thegiornalisti.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Fegato, Fegato Spappolato - Vasco Rossi.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Gli Alberi - Thegiornalisti.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Guaranteed - Eddie Vedder.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Happy - Pharrell Williams.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Here Comes The Hotstepper - Ini Kamoze.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Hostile - Erick Sermon Ft Keith Murray.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Hurt - Johnny Cash.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('I Still Haven''t Found What I''m Looking For - U2.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('I''m Gonna Be 500 Miles  -The Proclaimers.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Il Tuo Maglione Mio - Thegiornalisti.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Intro - M83.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Is This Love - Bob Marley and the Wailers.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Jamming - Bob Marley and the Wailers.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('L''Ultimo Grido Della Notte - Thegiornalisti.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('La Vie En Rose - Cristin Milioti.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Lovers in Japan - Coldplay.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Mad World - Gary Jules.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Midnight - Coldplay.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Midnight City - M83.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('My My Hey Hey - Neil Young.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('No Woman No Cry [Live] - Bob Marley and the Wailers.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Ogni Volta - Vasco Rossi.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('One Love - Bob Marley and the Wailers.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('One Love - Nas.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Punky Reggae Party - Bob Marley & the Wailers.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Raconte moi une histoire - M83.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Redemption Song - Bob Marley and the Wailers.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Round and Round - Imagine Dragons.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Satisfy My Soul - Bob Marley and the Wailers.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Sbagliare A Vivere - Thegiornalisti.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('September - Earth Wind & Fire.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Smile - David Gilmour.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('So Whatcha Sayin - EPMD.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Society - Eddie Vedder.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Sold Out - Thegiornalisti.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Soon, my friend - M83.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Stir It Up - Bob Marley and the Wailers.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('The Sound of Silence - Simon & Garfunkel.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('This bright flash - M83.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Three Little Birds  - Bob Marley and the Wailers.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Tra La Strada E Le Stelle - Thgiornalisti.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Up in Flames - Coldplay.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Uptown Girl - Billy Joel.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Vieni E Cambiami La Vita - Thegiornalisti.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Wait - M83.mp3', 'Sadness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Waiting In Vain - Bob Marley & the Wailers.mp3', 'Happiness');
+INSERT INTO `feel&drive`.songs (title, feeling) VALUES ('Xavier Rudd - Follow The Sun.mp3', 'Sadness');
+create table users
+(
+  username varchar(100) not null,
+  password varchar(50)  not null,
+  constraint users_username_uindex
+    unique (username)
+);
+
+alter table users
+  add primary key (username);
+
+INSERT INTO `feel&drive`.users (username, password) VALUES ('Aaron', 'baer');
+INSERT INTO `feel&drive`.users (username, password) VALUES ('admin', 'admin');
+
+create table relations
+(
+  title    varchar(150) not null,
+  username varchar(100) not null,
+  liked    varchar(10)  null,
+  feeling  varchar(20)  null,
+  primary key (title, username),
+  constraint relations_songs_title_fk
+    foreign key (title) references songs (title),
+  constraint relations_users_username_fk
+    foreign key (username) references users (username)
+);
