d284ba6351743ef322dc5ee384a54b2e7e951439
seandi <eandi.simo@outlook.it>
2019-06-07 19:05:21 +0200

    fix multiple triggers of push button

--- Pi-Code/custom_button.py
+++ Pi-Code/custom_button.py
@@ -1,17 +1,28 @@
 import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
+BUTTON_PIN = 18
+BOUNCETIME = 750
+
+# PULL_DOWN = 10000
+# PULL_UP = 220
+# V = 3.3
+# Iin = 0.3  # mV
+# Vh = 3.23
 
 
-# TODO: Simo check your circuit and modify effective pin of the demo!
-# demo to check effective working of the button
-def button_callback():
+def button_callback(channel):
     print("button pressed!")
 
 
-# change number of pin with the one effectively used; in this way the logic should not be inverted
-# Vhigh = button pressed
-
-GPIO.setwarnings(False)  # Ignore warning for now
+GPIO.setwarnings(False)  # Ignore warning
 GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(BUTTON_PIN, GPIO.IN)
+# Physical pull-down resistor is used otherwise use
+# GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback)
+GPIO.add_event_detect(BUTTON_PIN, GPIO.RISING, callback=button_callback, bouncetime=BOUNCETIME)
+
+# demo
+if __name__ == '__main__':
+    while True:
+        pass
+    GPIO.cleanup()
