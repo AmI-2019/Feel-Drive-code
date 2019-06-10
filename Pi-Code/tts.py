8a46b9843eb1fd1d8e2f9e048281b95bbd43f909
vina97 <vinai@hotmail.it>
2019-06-10 13:33:46 +0200

    fix path tts

--- Pi-Code/tts.py
+++ Pi-Code/tts.py
@@ -4,19 +4,19 @@ import os
 
 # custom functions
 def exception_audio():
-    os.system("mpg321 exception.mp3")
+    os.system("mpg321 /home/pi/tts/exception.mp3")
 
 
 def song_added():
-    os.system("mpg321 add.mp3")
+    os.system("mpg321 /home/pi/tts/add.mp3")
 
 
 def song_removed():
-    os.system("mpg321 remove.mp3")
+    os.system("mpg321 /home/pi/tts/remove.mp3")
 
 
 def start_speak():
-    os.system("mpg321 start_speak.mp3")
+    os.system("mpg321 /home/pi/tts/start_speak.mp3")
 
 
 def demo():
@@ -29,11 +29,11 @@ def demo():
 # initialization
 if __name__ == '__main__':
     tts = gTTS(text="I don't understand!", lang='en', )
-    tts.save("exception.mp3")
+    tts.save("/home/pi/tts/exception.mp3")
     tts = gTTS(text="song added from favourites", lang='en', )
-    tts.save("add.mp3")
+    tts.save("/home/pi/tts/add.mp3")
     tts = gTTS(text="song removed from favourites", lang='en', )
-    tts.save("remove.mp3")
+    tts.save("/home/pi/tts/remove.mp3")
     tts = gTTS(text="Feel & Drive, listening", lang='en', )
-    tts.save("start_speak.mp3")
+    tts.save("/home/pi/tts/start_speak.mp3")
     demo()
